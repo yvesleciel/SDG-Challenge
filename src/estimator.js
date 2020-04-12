3495eff39f026df8d614ef5f40d49f7b9eb7be40
leciel <yvangoudjou04@gmail.com>
2020-04-12 13:04:47 +0100

    correct bugg 5

--- src/estimator.js
+++ src/estimator.js
@@ -6,9 +6,9 @@ const covid19ImpactEstimator = (data) => {
   const factor = (days) => {
     let fact;
     if (days.periodType === 'days') {
-      fact = Math.trunc(1 / 3);
+      fact = 0;
     } if (days.periodType === 'weeks') {
-      fact = Math.trunc(7 / 3);
+      fact = 2;
     } if (days.periodType === 'months') {
       fact = Math.trunc(30 / 3);
     }
