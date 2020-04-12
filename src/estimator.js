3b3e3c618213deffbdeda1471c35f2d27929f486
leciel <yvangoudjou04@gmail.com>
2020-04-12 12:14:46 +0100

    challenge one two and three donne

--- src/estimator.js
+++ src/estimator.js
@@ -21,7 +21,11 @@ const covid19ImpactEstimator = (data) => {
       infectionsByRequestedTime: Math.trunc(currentlyInfected * (2 ** factor(input))),
       severeCasesByRequestedTime: Math.trunc(currentlyInfected * (2 ** factor(input)) * (15 / 100)),
       hospitalBedsByRequestedTime: Math.trunc(input.totalHospitalBeds * (35 / 100)
-        - currentlyInfected * (2 ** factor(input)) * (15 / 100))
+        - currentlyInfected * (2 ** factor(input)) * (15 / 100)),
+      casesForICUByRequestedTime: Math.trunc(currentlyInfected * (2 ** factor(input)) * (5 / 100)),
+      casesForVentilatorsByRequestedTime: Math.trunc(currentlyInfected * (2 ** factor(input))
+        * (2 / 100)),
+      dollarsInFlight: Math.trunc((currentlyInfected * (2 ** factor(input)) * 0.65 * 1.5) / 30)
     },
     severeImpact: {
       currentlyInfected: input.reportedCases * 50,
@@ -29,7 +33,11 @@ const covid19ImpactEstimator = (data) => {
       severeCasesByRequestedTime: Math.trunc(currentlyInfected2 * (2 ** factor(input))
         * (15 / 100)),
       hospitalBedsByRequestedTime: Math.trunc(input.totalHospitalBeds * (35 / 100)
-        - currentlyInfected2 * (2 ** factor(input)) * (15 / 100))
+        - currentlyInfected2 * (2 ** factor(input)) * (15 / 100)),
+      casesForICUByRequestedTime: Math.trunc(currentlyInfected2 * (2 ** factor(input)) * (5 / 100)),
+      casesForVentilatorsByRequestedTime: Math.trunc(currentlyInfected2
+        * (2 ** factor(input)) * (2 / 100)),
+      dollarsInFlight: Math.trunc((currentlyInfected2 * (2 ** factor(input)) * 0.65 * 1.5) / 30)
     }
   };
 };
