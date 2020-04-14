b54625fbf00e61acdbab489d5ea9d479163a7ea6
leciel <yvangoudjou04@gmail.com>
2020-04-14 11:17:01 +0100

    frontend ok

--- src/estimator.js
+++ src/estimator.js
@@ -32,8 +32,7 @@ const covid19ImpactEstimator = (data) => {
   const input = data;
   const currentlyInfected1 = Math.trunc(data.reportedCases * 10);
   const currentlyInfected2 = Math.trunc(data.reportedCases * 50);
-  // const ICU1 = currentlyInfected1 * (2 ** factor(data));
-  // const ICU2 = currentlyInfected2 * (2 ** factor(data));
+ 
 
   return {
     data: input,
